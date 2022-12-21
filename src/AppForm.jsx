259b32c112caed60682c7397131c4e1449e09383
jieun <lesson0186@gmail.com>
2022-12-21 23:04:11 +0900

    [basic]기본강의 전체코드

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function AppForm() {
-  const [form, setFrom] = useState({ name: '', email: '' });
+  //연관된 데이터라면 하나의 객체로 관리하는것이 더 효율적이다.
+  const [form, setFrom] = useState({ name: "", email: "" });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(form);
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
+    //⭐️객체의 key값을 동적으로 할당할 때에는 []형식으로 사용해야함
     setFrom({ ...form, [name]: value });
   };
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='name'>이름:</label>
+      <label htmlFor="name">이름:</label>
       <input
-        type='text'
-        id='name'
-        name='name'
+        type="text"
+        id="name"
+        name="name"
         value={form.name}
         onChange={handleChange}
       />
-      <label htmlFor='email'>이메일:</label>
+      <label htmlFor="email">이메일:</label>
       <input
-        type='email'
-        id='email'
-        name='email'
+        type="email"
+        id="email"
+        name="email"
         value={form.email}
         onChange={handleChange}
       />
